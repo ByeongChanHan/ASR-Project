3cf165678110a61e913dad3ca95674aba1f746cd
Byung Chan <gks5927@gmail.com>
2022-08-31 12:18:51 +0900

    setting changed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<App />} />
         <Route exact path="/about" element={<Manager />} />
